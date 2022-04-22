f6b32185ac45667afa46aae7bb8b13b4ac16b0d9
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-22 00:06:50 -0300

    remove unused import

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Navigate, NavLink } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import logo from "../logo.svg";
 
 export const Navigation = () => {
